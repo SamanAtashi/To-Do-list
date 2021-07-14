03048ed6dadce9b831941dc00ee2080af4e30c49
SamanAtashi <milady00126@gmail.com>
2021-07-14 13:49:14 +0300

    Add Basic Functionality to JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,54 @@
 import "./style.css";
 
-function component() {
-  const element = document.createElement("div");
+const list = [];
 
-  return element;
+// iterate array and show them in html
+let index = 1;
+
+function Build() {
+  const getAdd = document.getElementById("getAdd");
+
+  list.push({
+    description: getAdd.value,
+    completed: false,
+    index: index,
+  });
+  index++;
+
+  console.log(list);
 }
 
-document.body.appendChild(component());
+function showInList() {
+  const getAdd = document.getElementById("getAdd");
+  const show = document.getElementById("show");
+  console.log(show);
+  // add li element into the ul
+  // add class of item to it as well
+  const createLi = document.createElement("li");
+  createLi.classList.add("item");
+
+  const createCheckBox = document.createElement("input");
+  createCheckBox.type = "checkbox";
+  createCheckBox.name = "name";
+  createCheckBox.id = "checkboxId";
+  createCheckBox.classList.add("space-right");
+
+  let createLabel = document.createElement("label");
+  createLabel.htmlFor = "checkboxId";
+  createLabel.appendChild(document.createTextNode(getAdd.value));
+
+  createLi.appendChild(createCheckBox);
+  createLi.appendChild(createLabel);
+  show.appendChild(createLi);
+}
+
+const enterBtn = document.getElementById("getAdd");
+
+enterBtn.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    // document.getElementById('myBtn').click();
+    showInList();
+    Build();
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: './src/index.js',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-	],
-	devServer: {
-		contentBase: './dist',
-	},
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
